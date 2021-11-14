0cb98ee62de776b36acba1461eb5de106b34b7b5
romka05 <48714220+romka05@users.noreply.github.com>
2021-11-14 04:17:09 +0300

    Add files via upload

---
+++ web_crypton/js/main.js
@@ -0,0 +1,56 @@
+window.onscroll = function () {
+    myFunction()
+}
+
+    ;
+
+var header = document.getElementById("myMenu");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+        $(".header__menu").addClass("header__menu_bgcolor");
+
+    }
+
+    else {
+        header.classList.remove("sticky");
+        header.classList.remove("header__menu_bgcolor");
+    }
+}
+
+
+// стилизация инпутов формы name
+
+$('input.focused2').on('click', function () {
+    $('span.label2').removeClass('labelStart');
+    $('span.label2').addClass('labelSlide');
+});
+
+$('input.focused2').blur(function () {
+    if (!$.trim(this.value).length) {
+        $('span.label2').removeClass('labelSlide');
+        $('span.label2').addClass('labelStart');
+    }
+});
+
+// стилизация инпутов формы email
+
+$('input.emailFocused').on('click', function () {
+    $('spanlabelSecond').removeClass('labelFirst');
+    $('span.labelSecond').addClass('labelMoove');
+});
+
+$('input.emailFocused').blur(function () {
+    if (!$.trim(this.value).length) {
+        $('span.labelSecond').removeClass('labelMoove');
+        $('span.labelSecond').addClass('labelFirst');
+    }
+});
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu_links').toggleClass('active')
+    });
+});
